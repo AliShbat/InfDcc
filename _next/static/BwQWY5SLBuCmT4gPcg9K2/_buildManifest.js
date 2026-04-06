b9d1e490a840afed8f1484225cf6a85049cc4ed3
alishbat <ali.shbat96@gmail.com>
2026-04-06 19:45:45 +0300

    Updates

--- _next/static/BwQWY5SLBuCmT4gPcg9K2/_buildManifest.js
+++ _next/static/BwQWY5SLBuCmT4gPcg9K2/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/InfDcc//_next/:path+",
-        "destination": "/InfDcc/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
